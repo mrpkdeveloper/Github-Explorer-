1f7b32bfa2885150ff4cff6f1bfd12a416a2e374
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-13 18:54:41 +0530

    learned how to use ref to take input values from page

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,28 @@
 import React from 'react';
 
 class Search extends React.Component {
-    state = {
-        username: "",
+
+    inputref = React.createRef()
+    handleclick = () => {
+        const value = this.inputref.current.value
+        alert(`value inside input ref is  ${value}`)
     }
-    usernamechange = e => {
-        const value = e.target.value
-        this.setState({ username: value })
-    }
+    //this is done for taking input value from page
+    // state = {
+    //     username: "",
+    // }
+    // usernamechange = e => {
+    //     const value = e.target.value
+    //     this.setState({ username: value })
+    // }
+
     render() {
-        const { username } = this.state
+        // const { username } = this.state
         return (
             <div>
-                <input value={username} onChange={this.usernamechange} className="input" placeholder="Enter username" type="text" />
-                <button>Submit</button>
+                {/* <input value={username} onChange={this.usernamechange} className="input" placeholder="Enter username" type="text" /> */}
+                <input ref={this.inputref} className="input" placeholder="Enter username" type="text" />
+                <button onClick={this.handleclick}>Submit</button>
             </div>
         )
     }
