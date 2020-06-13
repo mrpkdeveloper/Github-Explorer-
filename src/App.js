4601657f112abca163545d2166704876688854df
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-13 23:43:40 +0530

    fetchdata function added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Search from './components/Search';
+class App extends React.Component {
+  state = {
+    user: null
+  }
 
-function App() {
-  return <Search />
+  fetchuser = (username) => {
+    //fetch user from github
+    alert(`function fetchdata called successfully with username: ${username}`)
+  }
+
+  render() {
+    return <Search fetchdata={this.fetchuser} />
+  }
 }
-
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,29 +2,33 @@ import React from 'react';
 
 class Search extends React.Component {
 
-    inputref = React.createRef()
-    handleclick = () => {
-        const value = this.inputref.current.value
-        alert(`value inside input ref is  ${value}`)
-    }
+    //input using ref 
+    // inputref = React.createRef()
+    // handleclick = () => {
+    //     const value = this.inputref.current.value
+    //     alert(`value inside input ref is  ${value}`)
+    // }
 
     //this is done for taking input value from page
     //this methdo is used when we want two way maping
-    // state = {
-    //     username: "",
-    // }
-    // usernamechange = e => {
-    //     const value = e.target.value
-    //     this.setState({ username: value })
-    // }
+    state = {
+        username: "",
+    }
+    usernamechange = e => {
+        const value = e.target.value
+        this.setState({ username: value })
+    }
 
     render() {
-        // const { username } = this.state
+        const { username } = this.state
+        const { fetchdata } = this.props
+
         return (
             <div>
-                {/* <input value={username} onChange={this.usernamechange} className="input" placeholder="Enter username" type="text" /> */}
-                <input ref={this.inputref} className="input" placeholder="Enter username" type="text" />
-                <button onClick={this.handleclick}>Submit</button>
+                {/* using ref */}
+                {/* <input ref={this.inputref} className="input" placeholder="Enter username" type="text" /> */}
+                <input value={username} onChange={this.usernamechange} className="input" placeholder="Enter username" type="text" />
+                <button onClick={() => { fetchdata(username) }}>Submit</button>
             </div>
         )
     }
