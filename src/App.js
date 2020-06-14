8326718e566bec73b60e5cd5774ac5f5d2564ebe
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-14 23:10:28 +0530

    fetch user data from github

--- src/App.js
+++ src/App.js
@@ -7,9 +7,21 @@ class App extends React.Component {
     user: null
   }
 
-  fetchuser = (username) => {
+  fetchuser = async (username) => {
     //fetch user from github
-    alert(`function fetchdata called successfully with username: ${username}`)
+    // alert(`function fetchdata called successfully with username: ${username}`)
+    try {
+      const res = await fetch(`https://api.github.com/users/${username}`,)
+      if (res.ok) {
+        const data = await res.json()
+        console.log(data)
+      }
+
+    }
+    catch (err) {
+      console.log(err)
+    }
+
   }
 
   render() {
