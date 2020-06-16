10cb20a24250eed6f01b77ce03cfd90c8e2607bb
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-16 20:04:11 +0530

    .

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     //fetch user from github
     // alert(`function fetchdata called successfully with username: ${username}`)
     try {
+      //using rest api github
       const res = await fetch(`https://api.github.com/users/${username}`,)
       if (res.ok) {
         const data = await res.json()
