364eb546b168e0f55880fe106c3c00e72be9653e
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-16 23:06:07 +0530

    handeled errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 import Search from './components/Search';
 class App extends React.Component {
   state = {
-    user: null
+    user: null,
+    error: null
+
   }
 
   fetchuser = async (username) => {
@@ -15,23 +17,41 @@ class App extends React.Component {
       const res = await fetch(`https://api.github.com/users/${username}`,)
       if (res.ok) {
         const data = await res.json()
-        // console.log(data)
+        console.log(data)
 
         //this will update internal state (basically saves the user data)
         return this.setState({
           user: data
         })
       }
+      // else {
+      const error = (await res.json()).message
+      this.setState({
+        error
+      })
+      // }
 
     }
     catch (err) {
-      console.log(err)
+      this.setState({
+        error: "there was some error"
+      })
+      // console.log(err)
     }
 
   }
 
   render() {
-    return <Search fetchdata={this.fetchuser} />
+
+    const { error } = this.state
+
+    return (
+      <div>
+        <Search fetchdata={this.fetchuser} />
+        {/* if error found then only  display para */}
+        {error && <p className="text-danger">{error}</p>}
+      </div>
+    )
   }
 }
 export default App;
