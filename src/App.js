83986040a46d84b8d681583983fedf46186b27de
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-16 23:23:10 +0530

    added loader to the file

--- src/App.js
+++ src/App.js
@@ -5,50 +5,61 @@ import Search from './components/Search';
 class App extends React.Component {
   state = {
     user: null,
-    error: null
+    error: null,
+    loading: false
 
   }
 
   fetchuser = async (username) => {
-    //fetch user from github
-    // alert(`function fetchdata called successfully with username: ${username}`)
-    try {
-      // using rest api github
-      const res = await fetch(`https://api.github.com/users/${username}`,)
-      if (res.ok) {
-        const data = await res.json()
-        console.log(data)
 
-        //this will update internal state (basically saves the user data)
-        return this.setState({
-          user: data
+    this.setState({ loading: true }, async () => {
+
+      //fetch user from github
+      // alert(`function fetchdata called successfully with username: ${username}`)
+      try {
+        // using rest api github
+        const res = await fetch(`https://api.github.com/users/${username}`,)
+        if (res.ok) {
+          const data = await res.json()
+          console.log(data)
+
+          //this will update internal state (basically saves the user data)
+          return this.setState({
+            user: data,
+            loading: false
+          })
+        }
+        // else {
+        const error = (await res.json()).message
+        this.setState({
+          error,
+          loading: false
         })
-      }
-      // else {
-      const error = (await res.json()).message
-      this.setState({
-        error
-      })
-      // }
+        // }
 
-    }
-    catch (err) {
-      this.setState({
-        error: "there was some error"
-      })
-      // console.log(err)
-    }
+      }
+      catch (err) {
+        this.setState({
+          error: "there was some error",
+          loading: false
+        })
+        // console.log(err)
+      }
+
+    })
+
 
   }
 
   render() {
 
-    const { error } = this.state
+    const { error, loading } = this.state
 
     return (
       <div>
         <Search fetchdata={this.fetchuser} />
         {/* if error found then only  display para */}
+        {loading && (<p>loading....</p>)}
         {error && <p className="text-danger">{error}</p>}
       </div>
     )
