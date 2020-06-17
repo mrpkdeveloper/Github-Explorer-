cf3a5f053255ba5fe1e040525c57702c6b28772e
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-17 23:45:53 +0530

    display repos functionality in progress

--- src/App.js
+++ src/App.js
@@ -13,62 +13,73 @@ class App extends React.Component {
 
   }
 
-  fetchuser = async (username) => {
-
-    this.setState({ loading: true }, async () => {
-
-      //fetch user from github
-      // alert(`function fetchdata called successfully with username: ${username}`)
-      try {
-        // using rest api github
-        const res = await fetch(`https://api.github.com/users/${username}`,)
-        if (res.ok) {
-          const data = await res.json()
-          console.log(data)
-
-          //this will update internal state (basically saves the user data)
-          return this.setState({
-            user: data,
-            loading: false
-          })
-        }
-        // else {
-        const error = (await res.json()).message
-        this.setState({
-          error,
-          loading: false
-        })
-        // }
-
-      }
-      catch (err) {
-        this.setState({
-          error: "there was some error",
-          loading: false
-        })
-        // console.log(err)
-      }
-
-    })
-
-
-  }
-
-  render() {
-
-    const { error, loading, user } = this.state
-
-    return (
-      <div>
-        <Search fetchdata={this.fetchuser} />
-        {/* if error found then only  display para */}
-        {loading && (<p>loading....</p>)}
-        {error && <p className="text-danger">{error}</p>}
-
-        {/* when there is no loading no error and user is present then render usercard component */}
-        {!loading && !error && user && <Usercard user={user} />}
-      </div>
-    )
+  fetchUSerData = async username => {
+    // using rest api github
+    const res = await fetch(`https://api.github.com/users/${username}`,)
+    if (res.ok) {
+      const data = await res.json()
+      console.log(data)
+      return { data }
+    }
+    // else {
+    const error = (await res.json()).message
+    return { error }
   }
 }
+
+
+
+
+fetchData = async username => {
+
+  this.setState({ loading: true }, async () => {
+
+    //fetch user from github
+    // alert(`function fetchdata called successfully with username: ${username}`)
+    try {
+
+
+      const { data, error } = await this.fetchUSerData(users)
+
+      if (data) {
+        return this.setState({
+          user: data, loading: false
+        })
+      }
+
+      this.setState({
+        error,
+        loading: false
+      })
+
+    } catch (err) {
+      this.setState({
+        error: "there was some error",
+        loading: false
+      })
+      // console.log(err)
+    }
+
+  })
+
+
+}
+
+render() {
+
+  const { error, loading, user } = this.state
+
+  return (
+    <div>
+      <Search fetchdata={this.fetchData} />
+      {/* if error found then only  display para */}
+      {loading && (<p>loading....</p>)}
+      {error && <p className="text-danger">{error}</p>}
+
+      {/* when there is no loading no error and user is present then render usercard component */}
+      {!loading && !error && user && <Usercard user={user} />}
+    </div>
+  )
+}
+
 export default App;
