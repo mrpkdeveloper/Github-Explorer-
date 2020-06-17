e50236195d343a98b92e7c5bfe54eedfc3064d20
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-17 22:30:58 +0530

    usercard.js file added

---
+++ src/components/usercard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+//we dont want here any state so we will make simple function component here and not class
+const usercard = (props) => {
+    return "user data"
+}
+
+export default usercard
