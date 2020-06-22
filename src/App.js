450a46007a07312f1b6a7e7586225dedf659d566
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-22 23:43:55 +0530

    error in userdata error and reposerror

--- src/App.js
+++ src/App.js
@@ -8,45 +8,57 @@ import Usercard from './components/Usercard';
 class App extends React.Component {
   state = {
     user: null,
-    error: null,
+    repos: [],
+    userDataError: null,
     loading: false
 
   }
 
   fetchUSerData = async (username) => {
     // using rest api github
-    const res = await fetch(`https://api.github.com/users/${username}`,)
+    //fetch user from github
+    // alert(`function fetchdata called successfully with username: ${username}`)
+    const res = await fetch(`https://api.github.com/users/${username}`)
     if (res.ok) {
       const data = await res.json()
       // console.log(data)
       return { data }
     }
-    // else {
+
     const error = (await res.json()).message
     return { error }
   }
 
 
-
+  fetchrepos = async (username) => {
+    const res = await fetch(`https://api.github.com/users/${username}/repos?page=1`)
+    if (res.ok) {
+      const data = await res.json()
+      // console.log(data)
+      return { data }
+    }
+    const error = (await res.json()).message
+    return { error }
+  }
 
 
   fetchData = async (username) => {
 
     this.setState({ loading: true }, async () => {
-
-      //fetch user from github
-      // alert(`function fetchdata called successfully with username: ${username}`)
       try {
 
+        const [user, repos] = await Promise.all([
+          this.fetchUSerData(username),
+          this.fetchrepos(username),
+        ])
 
-        const { data, error } = await this.fetchUSerData(users)
-
-        if (data) {
-          return this.setState({ user: data, loading: false })
+        if (user.data !== undefined && repos.data !== undefined) {
+          return this.setState({ user: user.data, repos: repos.data, loading: false })
         }
 
         this.setState({
-          error,
+          userDataError: user.error,
+          reposerror: repos.error,
           loading: false
         })
 
@@ -55,7 +67,6 @@ class App extends React.Component {
           error: "there was some error",
           loading: false
         })
-        // console.log(err)
       }
 
     })
@@ -72,10 +83,11 @@ class App extends React.Component {
         <Search fetchdata={this.fetchData} />
         {/* if error found then only  display para */}
         {loading && (<p>loading....</p>)}
-        {error && <p className="text-danger">{error}</p>}
+        {userDataError && <p className="text-danger">{userDataError}</p>}
 
         {/* when there is no loading no error and user is present then render usercard component */}
-        {!loading && !error && user && <Usercard user={user} />}
+        {!loading && !userDataError && user && <Usercard user={user} />}
+        {reposerror && <p className="text-danger">{reposerror}</p>}
       </div>
     )
   }
