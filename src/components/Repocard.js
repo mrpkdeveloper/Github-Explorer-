e1ec7134b6a349acdb1e91fabc19afba3225ddfd
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-23 18:04:48 +0530

    added repo link to repos

--- src/components/Repocard.js
+++ src/components/Repocard.js
@@ -3,13 +3,16 @@ import React from 'react';
 const Repocard = ({ repo }) =>
     <div className="card">
         <div className="card-body">
-            <h4 >{repo.full_name}</h4>
-            <p>
-                <strong>stars:</strong>{repo.stargazers_count}
-            </p>
-            <p>
-                <strong>watchers:</strong>{repo.watchers_count}
-            </p>
+            <a href={repo.html_url} target="blank">
+
+                <h4 >{repo.full_name}</h4>
+                <p>
+                    <strong>stars:</strong>{repo.stargazers_count}
+                </p>
+                <p>
+                    <strong>watchers:</strong>{repo.watchers_count}
+                </p>
+            </a>
         </div>
 
     </div>
